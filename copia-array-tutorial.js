a2b08031d8ef76956ca2011de5692b9bf8d6b800
SnSDk <sergiosnsdk@gmail.com>
2023-03-16 10:04:29 -0300

    copia-array-tutorial

---
+++ copia-array-tutorial.js
@@ -0,0 +1,26 @@
+
+
+const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+const copiaGatitos = [...gatitos]
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = gatitos.slice()
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = Array.from(gatitos)
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = gatitos.map (x=>x)
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = [].concat(gatitos)
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = JSON.parse(JSON.stringify(gatitos))
+
+// const gatitos = ['😺', '🐈', '🐈‍⬛', '🐾']
+// const copiaGatitos = []
+// for (let i = 0; i < gatitos.length; i++) {
+//     copiaGatitos[i] = gatitos[i];
+// }
+
